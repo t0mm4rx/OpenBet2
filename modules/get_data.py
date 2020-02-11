783bab4d9adf1d151f491f569003731365b73147
t0mm4rx <tom.marx@hotmail.fr>
2020-02-11 18:33:34 +0100

    Generate races

--- modules/get_data.py
+++ modules/get_data.py
@@ -2,6 +2,8 @@ import os
 import json
 import path
 import requests
+import numpy as np
+import path
 
 def get_programme(date, use_cache=True):
 	URL = "https://tablette.turfinfo.api.pmu.fr/rest/client/1/programme/{}".format(date)
@@ -27,9 +29,9 @@ def get_programme(date, use_cache=True):
 		print("Cannot download programme {}".format(date))
 		return -1
 
-def get_course(date, reunion, course, use_cache=True):
+def get_participants(date, reunion, course, use_cache=True):
 	URL = "https://tablette.turfinfo.api.pmu.fr/rest/client/1/programme/{}/R{}/C{}/participants".format(date, reunion, course)
-	FILE = "{}cache/courses/{}.json".format(path.PATH, "{}-{}-{}".format(date, reunion, course))
+	FILE = "{}cache/participants/{}.json".format(path.PATH, "{}-{}-{}".format(date, reunion, course))
 	if (use_cache):
 		if (os.path.exists(FILE)):
 			return json.loads(open(FILE, "r").read())
@@ -38,7 +40,7 @@ def get_course(date, reunion, course, use_cache=True):
 		result = requests.get(URL)
 	except:
 		print("Cannot download {} R{}C{}, retyring...".format(date, reunion, course))
-		return get_course(date, reunion, course)
+		return get_participants(date, reunion, course)
 	try:
 		result_json = json.loads(result.text)
 		if (not "participants" in result_json):
@@ -50,3 +52,42 @@ def get_course(date, reunion, course, use_cache=True):
 	except:
 		print("Cannot download {} R{}C{}".format(date, reunion, course))
 		return -1
+
+def get_course(date, reunion, course, use_cache=True):
+	FILE = "{}cache/courses/{}.json".format(path.PATH, "{}-{}-{}".format(date, reunion, course))
+	if (use_cache):
+		if (os.path.exists(FILE)):
+			return json.loads(open(FILE, "r").read())
+	programme = get_programme(date, use_cache)
+	if (programme == -1):
+		return -1
+	participants = get_participants(date, reunion, course, use_cache)
+	if (participants == -1):
+		return -1
+	try:
+		participants = participants["participants"]
+		course_obj = programme["reunions"][reunion - 1]["courses"][course - 1]
+		finish = course_obj["arriveeDefinitive"]
+		result = {
+			'finish': finish,
+			'horses': participants
+		}
+		if (finish):
+			finishOrder = list(np.array(course_obj["ordreArrivee"], dtype=np.int32).flatten())
+			result['result'] = course_obj["ordreArrivee"]
+		with open(FILE, "w+") as file:
+			file.write(json.dumps(result))
+		return result
+	except:
+		return -1
+
+def go_through_courses(fn):
+	for filename in os.listdir("{}cache/participants/".format(path.PATH)):
+		filename = filename.split(".")[0]
+		splits = filename.split("-")
+		date = splits[0]
+		reunion = int(splits[1])
+		course = int(splits[2])
+		obj = get_course(date, reunion, course)
+		if (obj != -1):
+			fn(obj)
